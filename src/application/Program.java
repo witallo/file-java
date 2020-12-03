fde6b9157f836b8e980b7edb8e60959a98f53b7f
Rick Witallo <ricklp@hotmail.com>
2020-12-03 14:54:11 -0300

    File try with resources

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,8 @@
 package application;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -11,16 +13,15 @@ public class Program {
 
 		File file = new File("C:\\temp\\text.txt");
 		Scanner sc = null;
-		try {
-			sc = new Scanner(file);
-			while (sc.hasNextLine()) {
-				System.out.println(sc.nextLine());
+		try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+			String line = br.readLine();
+			while (line != null) {
+				System.out.println(line);
+				line = br.readLine();
+
 			}
 		} catch (IOException e) {
 			System.out.println("Erro: " + e.getMessage());
-		} finally {
-			if (sc != null)
-				sc.close();
 		}
 
 	}
